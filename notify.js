f23f84212d055c020fc53bff947445db7d6509d7
ayyanar <ayyanar210420051@gmail.com>
2026-07-24 22:35:01 +0530

    full code

--- notify.js
+++ notify.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const nodemailer = require("nodemailer");
 
 const event = JSON.parse(
     fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8")
@@ -8,11 +9,38 @@ const data = {
     repository: process.env.GITHUB_REPOSITORY,
     branch: process.env.GITHUB_REF.replace("refs/heads/", ""),
     actor: process.env.GITHUB_ACTOR,
-    workflow: process.env.GITHUB_WORKFLOW,
     commitMessage: event.head_commit.message,
-    commitId: event.head_commit.id,
-    author: event.head_commit.author.name,
     timestamp: event.head_commit.timestamp
 };
 
-console.log(data);
+async function sendEmail() {
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    await transporter.sendMail({
+        from: process.env.EMAIL,
+        to: "ayyanar21042005@gmail.com",
+        subject: `Branch Updated: ${data.branch}`,
+        text: `
+Repository : ${data.repository}
+
+Branch : ${data.branch}
+
+Developer : ${data.actor}
+
+Commit : ${data.commitMessage}
+
+Time : ${data.timestamp}
+`
+    });
+
+    console.log("Email Sent Successfully");
+}
+
+sendEmail().catch(console.error);

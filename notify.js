121f189f447f10abb6b7050856ab4a5815674088
ayyanar <ayyanar210420051@gmail.com>
2026-07-24 21:24:45 +0530

    first git action mg get

--- notify.js
+++ notify.js
@@ -1,1 +1,13 @@
-console.log('hellp');
+const rf=requers("fs");
+const event=JSON.parse(rf.readFileSync(process.env.gethub_event_path,"utf8"));
+const data={
+    repository:process.env.gethub_repository,
+    branch:process.env.gethub_ref.replace("refs/heads/",""),
+    actor:process.env.gethub_actor,
+    workflow:process.env.gethub_workflow,
+    commitMessage: event.head_commit.message,
+    commitId: event.head_commit.id,
+    author: event.head_commit.author.name,
+    timestamp: event.head_commit.timestamp
+};
+console.log("Branch Notification Data:",data);
